404fa000693f370d90cc6e91a9d5bf08aab18c49
Kasper Riis Pedersen <kasper.riis.pedersen@gmail.com>
2020-09-25 19:26:30 +0200

    Change root view to custom blade template

---
+++ resources/views/app.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <link rel="stylesheet" href="/css/app.css">
+
+        <title>QuickBlog</title>
+
+
+    </head>
+    <body>
+        <noscript>
+        Denne side kræver, at JavaScript er slået til i din browser.
+        </noscript>
+        <div id="app">
+        </div>
+
+        <script src="/js/app.js"></script>
+    </body>
+    
+</html>
