9f25f27007304282508032c268fbeada3ab17a01
Kasper Riis Pedersen <kasper.riis.pedersen@gmail.com>
2020-09-27 16:58:16 +0200

    Create BlogPost list using data from backend

---
+++ resources/js/components/BlogPost.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card shadow m-4">
+        <div class="card-body blog-post-body">
+            <div class="info-line d-flex flex-row justify-content-between">
+                <h5 class="card-title mb-1">{{authorName}}</h5>
+                <h6 class="mb-1">{{timeStamp}}</h6>
+            </div>
+            <hr class="dashed mt-2 mb-2">
+            <p class="card-text">{{blogPostContent}}</p>
+        </div>
+    </div>
+</template>
+<script>
+import vagueTime from 'vague-time';
+
+export default {
+    props: {
+        authorName: String,
+        blogPostContent: String,
+        creationTime: Date,
+        lastUpdated: Date,
+    },
+    computed: {
+        timeStamp() {
+            return vagueTime.get({
+                from: new Date(),
+                to: this.lastUpdated
+            });
+        }
+    }
+}
+</script>
--- resources/js/components/BlogPostOverview.vue
+++ resources/js/components/BlogPostOverview.vue
@@ -4,15 +4,46 @@
             <a href="/" class="navbar-brand unselectable">QuickBlog</a>
         </nav>
         <div id="blogPostContainer" class="container">
+            <blog-post
+                v-for="post in sortedPosts"
+                v-bind="post"
+                :key="post.id"
+            ></blog-post>
         </div>
     </div>
 </template>
 <script>
+class BlogPost {
+    constructor({ id, content, created_at, updated_at, author }) {
+        this.id = id;
+        this.blogPostContent = content;
+        this.creationTime = new Date(created_at);
+        this.lastUpdated = new Date(updated_at);
+        this.authorName = author.name;
+    }
+}
 
 export default {
     data() {
         return {
+            blogPosts: [],
         }
     },
+    computed: {
+        sortedPosts() {
+            return this.blogPosts.sort((a, b) => {
+                return b.lastUpdated - a.lastUpdated;
+            });
+        },
+    },
+    created() {
+        axios.get('/api/blog_posts').then(response => {
+            let fetchedPosts = response.data;
+
+            fetchedPosts.forEach(post => {
+                this.blogPosts.push(new BlogPost(post));
+            });
+        });
+    }
 }
 </script>
