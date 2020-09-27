78052dbf63dbba64ae2d1e43cff67c8555eaa905
Kasper Riis Pedersen <kasper.riis.pedersen@gmail.com>
2020-09-27 16:50:32 +0200

    Create basic structure for BlogPostOverview page

---
+++ resources/js/components/BlogPostOverview.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="blogPostOverviewPage">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+            <a href="/" class="navbar-brand unselectable">QuickBlog</a>
+        </nav>
+        <div id="blogPostContainer" class="container">
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    data() {
+        return {
+        }
+    },
+}
+</script>
